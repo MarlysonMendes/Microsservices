86076fb09e083663eddcc777bd6ad5ef2a1bc21b
Marlyson Mendes <marlysonmendes@gmail.com>
2022-05-18 19:28:52 -0300

    Initial commit

---
+++ Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,33 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeendData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p=>true).Any();
+
+            if(!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPMyroducts());
+            }
+        }
+        private static IEnumerable<Product> GetPMyroducts()
+        {
+            return new List<Product>()
+            {
+                new Product ()
+                {
+                    Id = 1,
+                    Name = "Iphone X",
+                    Description = " Celular de ultima geracao",
+                    Image = "product-1.png",
+                    Price = 950.0M,
+                    Category = "smart phone"
+                }
+            };
+        }
+    }
+}
