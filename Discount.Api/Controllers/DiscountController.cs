6e32d9e7e61e97b0897777ac1ffa3fcf796755e6
Marlyson dos Santos Mendes <50031055+MarlysonMendes@users.noreply.github.com>
2022-05-31 01:59:26 -0300

    discount api

---
+++ Discount.Api/Controllers/DiscountController.cs
@@ -0,0 +1,43 @@
+﻿using Discount.Api.Entities;
+using Discount.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Discount.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _repository;
+
+        public DiscountController(IDiscountRepository repository)
+        {
+            _repository = repository;
+        }
+        [HttpGet("{productName}", Name = "GetDiscount")]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+        {
+            var coupon = await _repository.GetDiscount(productName);
+            return Ok(coupon);
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+        {
+            await _repository.CreateDiscount(coupon);
+            return CreatedAtRoute("GetDiscount", new { productName = coupon.ProductNanme }, coupon);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody] Coupon coupon)
+        {
+            return Ok(await _repository.UpdateDiscount(coupon));
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<Coupon>> DeleteDiscount(string productName)
+        {
+            return Ok(await _repository.DeleteDiscount(productName));
+        }
+    }
+}
---
+++ Discount.Api/Repositories/DiscountRepository.cs
@@ -0,0 +1,78 @@
+﻿using Dapper;
+using Discount.Api.Entities;
+using Npgsql;
+
+namespace Discount.Api.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public DiscountRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<bool> CreateDiscount(Coupon coupon)
+        {
+            NpgsqlConnection connection = GetConnection();
+
+            var affected = await connection.ExecuteAsync
+                ("INSERT INTO Coupon (ProductName, Description, Amount)" + 
+                "VALUES (@ProductName, @Description, @Amount",
+                new {ProductName = coupon.ProductNanme, Description = coupon.Description, Amount = coupon.Amount});
+            
+            if (affected == 0) return false;
+
+            return true;
+
+        }
+
+        public async Task<bool> DeleteDiscount(string productName)
+        {
+            NpgsqlConnection connection = GetConnection();
+
+            var affected = await connection.ExecuteAsync
+                ("DELETE FROM Coupon WHERE ProductName)" +
+                " = @ProductName",
+                new { ProductName = productName });
+
+            if (affected == 0) return false;
+
+            return true;
+        }
+
+        public async Task<Coupon> GetDiscount(string productName)
+        {
+;
+            NpgsqlConnection connection = GetConnection();
+
+            var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>
+                ("SELECT * FROM Coupon WHERE ProductName = @ProductName",
+                new {ProductName = productName});
+
+            if (coupon == null)
+                return new Coupon { ProductNanme = "No Discount", Amount = 0, Description="No Discount Desc" };
+            return coupon;
+        }
+
+        public async Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            NpgsqlConnection connection = GetConnection();
+
+            var affected = await connection.ExecuteAsync
+                ("INSERT INTO Coupon (ProductName, Description, Amount)" +
+                "VALUES (@ProductName, @Description, @Amount WHERE Id = @Id",
+                new { ProductName = coupon.ProductNanme, Description = coupon.Description, Amount = coupon.Amount, Id = coupon.Id });
+
+            if (affected == 0) return false;
+
+            return true;
+        }
+        private NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection
+                (_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        }
+    }
+}
